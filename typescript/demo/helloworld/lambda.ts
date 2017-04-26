647ffab2ecd32e7e9daf81843eb81fbeece9830d
Yann001 <1156203992@qq.com>
2017-04-26 22:40:52 +0800

    add basic knowledge about TypeScript

---
+++ typescript/demo/helloworld/lambda.ts
@@ -0,0 +1,33 @@
+/*
+var shape = {
+    name: "rectangle",
+    popup: function() {
+ 
+        console.log('This inside popup(): ' + this.name);
+ 
+        setTimeout(function() {
+            console.log('This inside setTimeout(): ' + this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+ 
+    }
+};
+ 
+shape.popup();
+*/
+
+var shape = {
+    name: "rectangle",
+    popup: function() {
+ 
+        console.log('This inside popup(): ' + this.name);
+ 
+        setTimeout( () => {
+            console.log('This inside setTimeout(): ' + this.name);
+            console.log("I'm a " + this.name + "!");
+        }, 3000);
+ 
+    }
+};
+ 
+shape.popup();
